f5c21b413a85b6368314fd52157f604ebb240f7c
Todd Ervin <servin@unca.edu>
2013-03-04 11:46:51 -0500

    worked on handler and mongo

--- app/streaming_tracker.py
+++ app/streaming_tracker.py
@@ -2,6 +2,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import pymongo
+import re
 from pymongo import MongoClient
 from pymongo import Connection 
 from pymongo.errors import ConnectionFailure
@@ -20,29 +21,44 @@ collection = db['tweet_test']
 
 class StdOutListener(StreamListener):
   
-  
+    
     def on_status(self, status):
-        # Prints the text of the tweet
        date = status.created_at.date().strftime("20%y/%m/%d") 
+       """
+       Trying to figure out regex syntax for python. Semmy wants Mongo to store and index 
+       the tweets by each Emoticon (the three below). Group the tweets by emoticon in the db.   
+       """
+       #smiley = re.finditer("'^:\))+$'", status.text)
+       #sad = 
+       #neutral = 
+       
+       # Prints the text of the tweet
        print(date + ' Tweet: ' + status.text)
 
 
     def on_error(self, error):
         print error 
 
-
+    """
+    This is my attempt at trying to handle the tweets. Basically, trying to get Mongo to save the 
+    tweet stream. The whole JSON object.  
+    """
     def handle_data(self, status):
         try:
             string_buffer = StringIO(status)
-            tweets = json.load(string_buffer) 
-        except Exception as ex:
-            print "Exception occurred: %s" % str(ex)
-        try:
-            self.raw_tweets.insert(tweets)
-        except Exception as ex:
-            print "Exception occurred: %s" % str(ex)  
-
-
+            tweets = json.load(string_buffer)
+            i = 0
+            while True:
+              i = i + 1
+              for tweet in tweets:
+                try:
+                  print ' SAVING: "%s"' % tweet['text']
+                  collection.insert(tweet)
+                except Exception as e:
+                  print 'ERROR: %s' % e 
+        except Exception as e:
+           print "Exception occurred: %s" % str(e)
+   
 
 
 if __name__ == '__main__':
