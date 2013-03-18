a8224d09063bfc029e23c6022d7ea1bc9ed8cda6
Todd Ervin <servin@unca.edu>
2013-03-18 19:09:57 -0400

    worked on tracker file

--- app/streaming_tracker.py
+++ app/streaming_tracker.py
@@ -2,10 +2,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import pymongo
-from pymongo import MongoClient
 from pymongo import Connection 
 from pymongo.errors import ConnectionFailure
-connection = MongoClient()
 import credentials
 import re
 import json
@@ -22,32 +20,24 @@ db = conn['rad']
 
 class StdOutListener(StreamListener):
   
-    #tweet object
+    #tweets and MongoDB
     def on_status(self, status):
        date = status.created_at.date().strftime("20%y/%m/%d")       
        # Prints the text of the tweet
        print(date + ' Tweet: ' + status.text)
-       data = json.loads(status)
-       db.what_up.insert = ({'name': data['user']['screen_name'],
-                            'text': data['text']})
-
+       #regex for each emoticon
+       #smiley = re.finditer("'^:\))+$'", status.text)
+       #sad = 
+       #neutral =   
+       #data = json.loads(status)
+       #db.what_up.insert = ({'name': data['user']['screen_name'],
+                             #'text': data['text']})
     
     #error handling
     def on_error(self, error):
         print error 
     
-    
-    #regex for each emoticon for MongoDB
-    #def emoticon(self, status):
-    """
-    Trying to figure out regex syntax for python. Semmy wants Mongo to store and index 
-    the tweets by each Emoticon (the three below). Group the tweets by emoticon in the db.
-    """   
-       #smiley = re.finditer("'^:\))+$'", status.text)
-       #sad = 
-       #neutral = 
-
-    
+     
 
 
 if __name__ == '__main__':
