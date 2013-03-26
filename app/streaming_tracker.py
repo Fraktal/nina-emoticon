e2876886775e40c5389819a933af22eb6214dcbf
Todd Ervin <servin@unca.edu>
2013-03-26 15:25:14 -0400

    removed practice.py

--- app/streaming_tracker.py
+++ app/streaming_tracker.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import pymongo
 from pymongo import Connection 
-from pymongo.errors import ConnectionFailure
 import credentials
+import simplejson as json
 import re
-import json
+
 
 #credentials for Twitter OAuth 
 CONSUMER_KEY = credentials.CONSUMER_KEY
@@ -14,27 +14,38 @@ CONSUMER_SECRET = credentials.CONSUMER_SECRET
 ACCESS_TOKEN = credentials.ACCESS_TOKEN
 ACCESS_TOKEN_SECRET = credentials.ACCESS_TOKEN_SECRET
 
-#mongo connection
+#Mongo connection
 conn = pymongo.Connection('localhost', 27017)
 db = conn['rad']
 
 
 class StdOutListener(StreamListener):
   
-    #tweets and MongoDB
+    #tweets and Mongo
     def on_status(self, status):
        date = status.created_at.date().strftime("20%y/%m/%d") #readable date for tweets       
        print(date + ' Tweet: ' + status.text)
+
+    buffer = ""
        
+    def on_receive(status):
+       buffer += data.strip()         
+       if (data.endswith("\r\n")):
+         if buffer:
+           on_status(buffer)
+           buffer = ""  
+  
        #regex for each emoticon
        #smiley = re.finditer("'^:\))+$'", status.text)
        #sad = 
        #neutral =   
        
        #JSON and Mongo
-       #data = json.loads(status)
-       #db.what_up.insert = ({'name': data['user']['screen_name'],
-                             #'text': data['text']})
+       data = json.loads(status)
+       tweet = json.dumps(dict(data))
+       tweet_data = {"date": "date", "tweet": "tweet.text"}
+       #save tweets: date and text
+       db.what_up.save = (tweet_data)
     
     #error handling
     def on_error(self, error):
