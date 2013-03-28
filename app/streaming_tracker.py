23d70a4a9e72f7562875d59da2be5ca7acd8190e
Todd Ervin <servin@unca.edu>
2013-03-28 15:14:13 -0400

    setting up emoticon as keyword in mongo

--- app/streaming_tracker.py
+++ app/streaming_tracker.py
@@ -23,11 +23,27 @@ class StdOutListener(StreamListener):
   
     #tweets and Mongo
     def on_status(self, status):
-       date = status.created_at.date().strftime("20%y/%m/%d") #readable date for tweets       
-       # print(date + ' Tweet: ' + status.text)
+       #readable date for tweets
+       date = status.created_at.date().strftime("20%y/%m/%d")        
+       
+       #pickle the BSON data into JSON
        data = json.loads(jsonpickle.encode(status))
        print data
+       
+       #store the whole tweet object
        db.tweets.save({"Date": date, "tweet": data})
+
+       """ possible way to store by emoticon
+       
+       if(regex = smiley):
+          db.tweets.save({"smiley": ":)", "Date": date, "tweet": data})
+       elif(regex = sad):
+          db.tweets.save({"sad": ":(", "Date": date, "tweet": data})
+       else(regex = neutral):
+          db.tweets.save({"neutral": ":|", "Date": date, "tweet": data}) 
+
+
+       """
 
     #error handling
     def on_error(self, error):
