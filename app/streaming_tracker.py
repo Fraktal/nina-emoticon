e0eb3a4d690bb44770e9a4e5e505f638f38b1706
Todd Ervin <servin@unca.edu>
2013-03-29 01:26:37 -0400

    finished streaming_tracker file

--- app/streaming_tracker.py
+++ app/streaming_tracker.py
@@ -1,8 +1,11 @@
+#! /usr/bin/python
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import pymongo
 from pymongo import Connection 
+import sys
 import credentials
 import json
 import jsonpickle
@@ -23,41 +26,47 @@ class StdOutListener(StreamListener):
   
     #tweets and Mongo
     def on_status(self, status):
+     
+      try:
+         #readable date for tweets
+         date = status.created_at.date().strftime("20%y/%m/%d") 
        
-       #readable date for tweets
-       date = status.created_at.date().strftime("20%y/%m/%d") 
+         #print text of tweets
+         print status.text       
        
-       #print text of tweets
-       print status.text       
-       
-       #store the whole tweet object by emoticon
-       if re.search('(:\))', status.text):
-          data = json.loads(jsonpickle.encode(status))
-          db.tweets.save({"smiley": ":)", "Date": date, "tweet": data})
-       elif re.search('(:\()', status.text):
-          data = json.loads(jsonpickle.encode(status))
-          db.tweets.save({"sad": ":(", "Date": date, "tweet": data})
-       else:
-          data = json.loads(jsonpickle.encode(status))
-          db.tweets.save({"neutral": ":|", "Date": date, "tweet": data})      
+         #store the whole tweet object by emoticon
+         if re.search('(:\))', status.text):
+            data = json.loads(jsonpickle.encode(status))
+            db.tweets.save({"smiley": ":)", "date": date, "tweet": data})
+         elif re.search('(:\()', status.text):
+            data = json.loads(jsonpickle.encode(status))
+            db.tweets.save({"sad": ":(", "date": date, "tweet": data})
+         else:
+            data = json.loads(jsonpickle.encode(status))
+            db.tweets.save({"neutral": ":|", "date": date, "tweet": data})  
 
-    #count the number of tweets in database and print it
-    smiley_count = db.tweets.find({"smiley": ":)"}).count()
-    sad_count = db.tweets.find({"sad": ":("}).count()
-    neutral_count = db.tweets.find({"neutral": ":|"}).count()
-    total_count = db.tweets.count()
-    print "Total number of tweets: ", total_count
-    print "Smiley ", smiley_count
-    print "Sad ", sad_count
-    print "Neutral ", neutral_count
+      except KeyboardInterrupt:
+          print "Exiting"
+                
 
     #error handling
     def on_error(self, error):
         print error 
+
+
+#count the number of tweets in mongo and print it
+total_count = db.tweets.count()
+smiley_count = db.tweets.find({"smiley": ":)"}).count()
+sad_count = db.tweets.find({"sad": ":("}).count()
+neutral_count = db.tweets.find({"neutral": ":|"}).count()
+  
+print "Total tweets: ", total_count
+print "Smiley tweets: ", smiley_count
+print "Sad tweets: ", sad_count
+print "Neutral tweets: ", neutral_count
+
     
      
-
-
 if __name__ == '__main__':
     listener = StdOutListener()
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
