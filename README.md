b675001e90bfbe6b2d99cd7ac1b6d99bfe8d5be4
Todd Ervin <servin@unca.edu>
2013-04-08 08:53:11 -0400

    cleaning up

---
+++ README.md
@@ -0,0 +1,8 @@
+NINA LAB
+========
+
+
+The **emoticon-project** is a sentiment analysis web app using machine learning methods such as K-means and K nearest neighbor to link tweets with emoticons to track sentiment
+
+This code is being developed by the NINA Lab at the **University of North Carolina Asheville**. This application
+is in development. Please be aware of this as you look at the code. 
---
+++ app/streaming_tracker.py
@@ -0,0 +1,88 @@
+#! /usr/bin/python
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import pymongo
+from pymongo import Connection 
+import sys
+import credentials
+import json
+import jsonpickle
+import re
+
+#credentials for Twitter OAuth 
+CONSUMER_KEY = credentials.CONSUMER_KEY
+CONSUMER_SECRET = credentials.CONSUMER_SECRET
+ACCESS_TOKEN = credentials.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = credentials.ACCESS_TOKEN_SECRET
+
+#Mongo connection
+conn = pymongo.Connection('localhost', 27017)
+db = conn['tweetDB']
+
+
+class StdOutListener(StreamListener):
+  
+    #tweets and Mongo
+    def on_status(self, status):
+     
+      try:
+
+         #simplified and readable date for the tweets
+         date = status.created_at.date().strftime("20%y/%m/%d")      
+
+         #jsonpickle defines complex Python model objects and turns the objects into JSON 
+         data = json.loads(jsonpickle.encode(status))
+       
+         #store the whole tweet object by emoticon
+         if re.search('(:\))', status.text):
+            db.tweets.save({"smiley": ":)", "date": date, "tweet": data, 
+                            "tweet_text_smiley": status.text})
+
+         elif re.search('(:\()', status.text):
+            db.tweets.save({"sad": ":(", "date": date, "tweet": data,
+                            "tweet_text_sad": status.text})
+
+         else:
+            db.tweets.save({"neutral": ":|", "date": date, "tweet": data,
+                            "tweet_text_neutral": status.text}) 
+
+      except ConnectionFailure, e:
+          sys.stderr.write("could not connect to MongoDB: %s" % e)
+          sys.exit(1)     
+                
+
+    #error handling
+    def on_error(self, error):
+        print error 
+
+
+#count the number of tweets in mongo and print it
+total_count = db.tweets.count()
+smiley_count = db.tweets.find({"smiley": ":)"}).count()
+sad_count = db.tweets.find({"sad": ":("}).count()
+neutral_count = db.tweets.find({"neutral": ":|"}).count()
+
+print
+print "Starting connection to Twitter Streaming API....  " 
+print
+print "This is the last count from last connection in mongo: "
+
+print "Total tweets: ", total_count
+print "Smiley tweets: ", smiley_count
+print "Sad tweets: ", sad_count
+print "Neutral tweets: ", neutral_count
+print
+print "retrieving data......"
+
+    
+     
+if __name__ == '__main__':
+    listener = StdOutListener()
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET )
+    stream = Stream(auth, listener)    
+    stream.filter(track=[':)', ':(', ':|'])
+    
+      
